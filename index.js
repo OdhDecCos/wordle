c296c80150c2cdafd2256280c8d8341df017bb67
OdhDecCos <71221869+OdhDecCos@users.noreply.github.com>
2022-07-14 14:15:02 +0200

    Fix async fetch function

--- index.js
+++ index.js
@@ -1,37 +1,29 @@
 const grid = document.querySelector("#grid");
 const keys = document.querySelectorAll(".key");
 const tiles = document.querySelectorAll(".tile");
+
+let keyObj = {};
 let pos = 0;
 let line = 0;
 let word = "";
 let goal = "liver";
 
-function checkWord(word) {
-    let isWord = true;
-    fetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + word)
-    .then(res =>  {
-        if (!res.ok) {
-            isWord = false;
-        }
-    })
-    return isWord;
-}
-
 function colourTiles(word, line, keyNum) {
     for(let i = 0; i < word.length; i++) {
         if (word[i] === goal[i]) {
             tiles[line*5 + i].setAttribute("data-tile", "correct");
-            keys[keyNum].setAttribute("data-tile", "correct");
+            keys[keyObj[word[i]]].setAttribute("data-tile", "correct");
         } else if (goal.includes(word[i])) {
             tiles[line*5 + i].setAttribute("data-tile", "present");
+            keys[keyObj[word[i]]].setAttribute("data-tile", "present");
         } else {
             tiles[line*5 + i].setAttribute("data-tile", "absent");
-            keys[keyNum].setAttribute("data-tile", "absent");
+            keys[keyObj[word[i]]].setAttribute("data-tile", "absent");
         }
     }
 }
 
-function clicked(keyNum) {
+async function clicked(keyNum) {
     const key = keys[keyNum].innerHTML;
     if (key === "backspace") {
         if (pos === line * 5) { return; }
@@ -40,7 +32,12 @@ function clicked(keyNum) {
         tiles[pos].innerHTML = "";
     } else if (key === "enter") {
         if (pos !== ((line + 1) * 5) || line === 5) { return; }
-        if (checkWord(word)) {
+        let isWord;
+        await fetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + word)
+        .then(res => {
+            if (res.ok) { isWord = true; } else { isWord = false;}
+        });
+        if (isWord) {
             colourTiles(word, line, keyNum);
             if (word === goal) {
                 alert("Congrats.");
@@ -66,4 +63,5 @@ function clicked(keyNum) {
 
 for (let i = 0; i < keys.length; i++) {
     keys[i].onclick = function () { clicked(i) };
+    keyObj[keys[i].innerHTML] = i;
 }
