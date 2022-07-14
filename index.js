2b6b7c1291eb4425490a0f5009b2941cfb948481
OdhDecCos <71221869+OdhDecCos@users.noreply.github.com>
2022-07-14 15:11:48 +0200

    Add keyboard support and toast messages

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const grid = document.querySelector("#grid");
 const keys = document.querySelectorAll(".key");
 const tiles = document.querySelectorAll(".tile");
+const toastBar = document.querySelector(".toast");
+const message = document.querySelector(".message");
 
+let win = false;
 let keyObj = {};
 let pos = 0;
 let line = 0;
@@ -9,21 +12,22 @@ let word = "";
 let goal = "liver";
 
 function colourTiles(word, line, keyNum) {
-    for(let i = 0; i < word.length; i++) {
+    for (let i = 0; i < word.length; i++) {
         if (word[i] === goal[i]) {
-            tiles[line*5 + i].setAttribute("data-tile", "correct");
+            tiles[line * 5 + i].setAttribute("data-tile", "correct");
             keys[keyObj[word[i]]].setAttribute("data-tile", "correct");
         } else if (goal.includes(word[i])) {
-            tiles[line*5 + i].setAttribute("data-tile", "present");
+            tiles[line * 5 + i].setAttribute("data-tile", "present");
             keys[keyObj[word[i]]].setAttribute("data-tile", "present");
         } else {
-            tiles[line*5 + i].setAttribute("data-tile", "absent");
+            tiles[line * 5 + i].setAttribute("data-tile", "absent");
             keys[keyObj[word[i]]].setAttribute("data-tile", "absent");
         }
     }
 }
 
 async function clicked(keyNum) {
+    if (win) { return; }
     const key = keys[keyNum].innerHTML;
     if (key === "backspace") {
         if (pos === line * 5) { return; }
@@ -31,20 +35,31 @@ async function clicked(keyNum) {
         word = word.slice(0, -1);
         tiles[pos].innerHTML = "";
     } else if (key === "enter") {
-        if (pos !== ((line + 1) * 5) || line === 5) { return; }
+        if (pos !== ((line + 1) * 5)) { 
+            message.innerHTML = 'Not enough letters';
+            toastBar.setAttribute("data-visible", true);
+            setTimeout(function () { toastBar.setAttribute("data-visible", false); }, 1200);
+            return;
+        }
         let isWord;
         await fetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + word)
-        .then(res => {
-            if (res.ok) { isWord = true; } else { isWord = false;}
-        });
+            .then(res => {
+                if (res.ok) { isWord = true; } else { isWord = false; }
+            });
         if (isWord) {
             colourTiles(word, line, keyNum);
             if (word === goal) {
-                alert("Congrats.");
+                win = true;
+            } else if (line === 5) {
+                message.innerHTML = goal.toUpperCase();
+                toastBar.setAttribute("data-visible", true);
+                return;
             }
             line++;
         } else {
-            alert("Not a word.");
+            message.innerHTML = 'Not in word list';
+            toastBar.setAttribute("data-visible", true);
+            setTimeout(function () { toastBar.setAttribute("data-visible", false); }, 1200);
             word = "";
             for (let i = 0; i < 5; i++) {
                 pos--;
@@ -65,3 +80,17 @@ for (let i = 0; i < keys.length; i++) {
     keys[i].onclick = function () { clicked(i) };
     keyObj[keys[i].innerHTML] = i;
 }
+
+window.addEventListener("keydown", function (event) {
+    if (event.defaultPrevented) {
+        return;
+    }
+
+    let press = event.key.toLowerCase();
+
+    if (press === 'enter' || press === 'backspace'
+        || (press >= "a" && press <= 'z' && press.length === 1)) {
+        clicked(keyObj[press]);
+    }
+    event.preventDefault();
+}, true);
